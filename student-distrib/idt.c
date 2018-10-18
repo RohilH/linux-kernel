4c7fe0dbc003dba351147a9091de1731088748a4
Iyengar <ibi2@illinois.edu>
2018-10-17 23:51:50 -0500

    Initial Commit for idt.c and idt.h

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,3 +1,24 @@
 //////
 // SPACE FOR IDT. Here we will propagate the idt with the appropriate vectors
 //////
+
+#include "x86_desc.h"
+#include "idt.h"
+
+void propagateIDTEntry(x86_desc_t idt_desc_ptr) {
+  idt_load();
+  int x;
+  for (int x = 0; x<NUM_VEC; x++) {
+    idt[x].offset_15_00 = 0;
+    idt[x].seg_selector = 0;
+    idt[x].reserved4    = 0;
+    idt[x].reserved3    = 0;
+    idt[x].reserved2    = 0;
+    idt[x].reserved1    = 0;
+    idt[x].size         = 0;
+    idt[x].reserved0    = 0;
+    idt[x].dpl          = 0;
+    idt[x].present      = 0;
+    idt[x].offset_31_16 = 0;
+  }
+}
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -1,3 +1,12 @@
 //////
 // SPACE FOR IDT. Here we will propagate the idt with the appropriate vectors
 //////
+
+#ifndef _IDT_H
+#define _IDT_H
+
+#include "x86_desc.h"
+
+void propagateIDTEntry(x86_desc_t idt_desc_ptr);
+
+#endif /* _IDT_H */
