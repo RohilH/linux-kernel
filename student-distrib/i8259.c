c88dedddf5f110a1cbd7184b5844daf372c54688
Iyengar <ibi2@illinois.edu>
2018-10-18 19:37:19 -0500

    IRQ initialization started, code for that is from the actual linux kernel.

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -9,8 +9,79 @@
 uint8_t master_mask; /* IRQs 0-7  */
 uint8_t slave_mask;  /* IRQs 8-15 */
 
+// /* macro used to write a byte to a port */
+// #define OUTB(port, val)                                 \
+// do {                                                    \
+//     asm volatile("                                    \n\
+//         OUTB %b1, (%w0)                               \n\
+//         "                                               \
+//         : /* no outputs */                              \
+//         : "d"((port)), "a"((val))                       \
+//         : "memory", "cc"                                \
+//     );                                                  \
+// } while (0)
+
+
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+  //
+  // unsigned long flags;
+  //
+	// /* initialize the controller */
+	// raw_spin_lock_irqsave(&i8259_lock, flags);
+  //
+	// /* Mask all first */
+	// OUTB(0xff, 0xA1);
+	// OUTB(0xff, 0x21);
+  //
+	// /* init master interrupt controller */
+	// OUTB(0x11, 0x20); /* Start init sequence */
+	// OUTB(0x00, 0x21); /* Vector base */
+	// OUTB(0x04, 0x21); /* edge triggered, Cascade (slave) on IRQ2 */
+	// OUTB(0x01, 0x21); /* Select 8086 mode */
+  //
+	// /* init slave interrupt controller */
+	// OUTB(0x11, 0xA0); /* Start init sequence */
+	// OUTB(0x08, 0xA1); /* Vector base */
+	// OUTB(0x02, 0xA1); /* edge triggered, Cascade (slave) on IRQ2 */
+	// OUTB(0x01, 0xA1); /* Select 8086 mode */
+  //
+	// /* That thing is slow */
+	// udelay(100);
+  //
+	// /* always read ISR */
+	// OUTB(0x0B, 0x20);
+	// OUTB(0x0B, 0xA0);
+  //
+	// /* Unmask the internal cascade */
+	// cached_21 &= ~(1 << 2);
+  //
+	// /* Set interrupt masks */
+	// OUTB(cached_A1, 0xA1);
+	// OUTB(cached_21, 0x21);
+  //
+	// raw_spin_unlock_irqrestore(&i8259_lock, flags);
+  //
+	// /* create a legacy host */
+	// i8259_host = irq_domain_add_legacy_isa(node, &i8259_host_ops, NULL);
+	// if (i8259_host == NULL) {
+	// 	printk(KERN_ERR "i8259: failed to allocate irq host !\n");
+	// 	return;
+	// }
+  //
+	// /* reserve our resources */
+	// /* XXX should we continue doing that ? it seems to cause problems
+	//  * with further requesting of PCI IO resources for that range...
+	//  * need to look into it.
+	//  */
+	// request_resource(&ioport_resource, &pic1_iores);
+	// request_resource(&ioport_resource, &pic2_iores);
+	// request_resource(&ioport_resource, &pic_edgectrl_iores);
+  //
+	// if (intack_addr != 0)
+	// 	pci_intack = ioremap(intack_addr, 1);
+  //
+	// printk(KERN_INFO "i8259 legacy interrupt controller initialized\n");
 }
 
 /* Enable (unmask) the specified IRQ */
