047a7036e0185762298c350b2c7b9f34b737d8e6
Jeng <mjeng2@illinois.edu>
2018-10-20 16:36:32 -0500

    keyboard handler and began rtc marcos

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -5,8 +5,7 @@
 uint32_t status;
 uint32_t scanCode;
 uint32_t prevScanCode;
-int shift;
-int caps;
+int shift, caps, ctrl, alt;
 
 /* https://wiki.osdev.org/PS/2_Keyboard#Scan_Code_Set_1 */
 
@@ -33,38 +32,41 @@ static char scanCodeToChar[3][60] = {{'\0', '\0', '1', '2', '3', '4', '5', '6', 
                                      '*', '\0', ' ', '\0'}};
 
 void KEYBOARD_INIT() {
+  cli();
   enable_irq(1);
   scanCode = 0;
   prevScanCode = 0;
-  shift = 0;
-  caps = 0;
+  shift = 0, caps = 0, ctrl = 0, alt = 0;
   clear();
+  sti();
 }
 
 void KEYBOARD_HANDLER() {
-  while (1) {
-    status = inb(0x64);
-    if (status&0x1) {
-      scanCode = inb(0x60);
-      if (scanCode!=0 && scanCode<0x80) {
-        if (prevScanCode!=scanCode) {
-          if (shift==1)
-            printf("%c", scanCodeToChar[2][scanCode]);
-          else if (caps==1)
-            printf("%c", scanCodeToChar[1][scanCode]);
-          else
-            printf("%c", scanCodeToChar[0][scanCode]);
-        }
-      }
-      if (prevScanCode==0x3A && scanCode == 0xBA) {
-        if (caps==0) caps=1;
-        else caps=0;
-      }
+  asm("pusha");
+  cli();
 
-      if (scanCode==0x2A || scanCode==0x36) shift = 1;
-      if (scanCode==0xAA || scanCode==0xB6) shift = 0;
-      prevScanCode = scanCode;
+  scanCode = inb(0x60);
+  if (scanCode!=0 && scanCode<0x80) {
+    if (prevScanCode!=scanCode) {
+      if (shift==1)
+        printf("%c", scanCodeToChar[2][scanCode]);
+      else if (caps==1)
+        printf("%c", scanCodeToChar[1][scanCode]);
+      else
+        printf("%c", scanCodeToChar[0][scanCode]);
     }
   }
+  if (prevScanCode==0x3A && scanCode == 0xBA) {
+    if (caps==0) caps=1;
+    else caps=0;
+  }
+
+  if (scanCode==0x2A || scanCode==0x36) shift = 1;
+  if (scanCode==0xAA || scanCode==0xB6) shift = 0;
+  prevScanCode = scanCode;
+
   send_eoi(1);
+
+  sti();
+  asm("popa");
 }
--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -5,43 +5,22 @@
 #define status_register_a 0x8A
 #define status_register_b 0x8B
 
-#define OUTB(port, val)                                 \
-do {                                                    \
-    asm volatile("                                    \n\
-        outb %b1, (%w0)                               \n\
-        "                                               \
-        : /* no outputs */                              \
-        : "d"((port)), "a"((val))                       \
-        : "memory", "cc"                                \
-    );                                                  \
-} while (0)
-
-//TAKEN FROM OSDEV
-static inline uint8_t INB(uint16_t port)
-{
-    uint8_t ret;
-    asm volatile ( "inb %1, %0"
-                   : "=a"(ret)
-                   : "Nd"(port) );
-    return ret;
-}
-
 void RTC_INIT() {
   // SRC: https://wiki.osdev.org/RTC
   cli(); // Disable interrupts
-  OUTB(0x70, 0x8A);	// select Status Register A, and disable NMI (by setting the 0x80 bit)
-  OUTB(0x71, 0x20);	// write to CMOS/RTC RAM
+  outb(0x70, 0x8A);
+  outb(0x71, 0x20);
 
-  OUTB(0x70, 0x8B); // select register B, and disable NMI
-  uint8_t prev = INB(0x71);	// read the current value of register B
-  OUTB(0x70, 0x8B); // set the index again (a read will reset the index to register D)
-  OUTB(0x71, prev | 0x40); // write the previous value ORed with 0x40. This turns on bit 6 of register B
+  outb(0x70, 0x8B); // Status Register B / Disable NMI
+  uint8_t prev = inb(0x71);	// Register B value
+  outb(0x71, prev | 0x40); // ORed with 0x40: Register B
+  outb(0x70, 0x8B); // Reset idx
 
   uint8_t rate = 0x0F; // rate must be above 2 and not over 15
-  OUTB(0x70, 0x8A); // set index to register A, disable NMI
-  prev = INB(0x71); // get initial value of register A
-  OUTB(0x70, 0x8A); // reset index to A
-  OUTB(0x71, (prev & 0xF0) | rate); //write only our rate to A. Note, rate is the bottom 4 bits.
+  outb(0x70, 0x8A); // set index to register A, disable NMI
+  prev = inb(0x71); // get initial value of register A
+  outb(0x71, (prev & 0xF0) | rate); //write only our rate to A. Note, rate is the bottom 4 bits.
+  outb(0x70, 0x8A); // reset index to A
 
   enable_irq(8); // Enable RTC IRQ
   sti(); // Enable interrupts
@@ -49,11 +28,11 @@ void RTC_INIT() {
 
 void RTC_HANDLER() {
   printf("RTC INTERRUPT");
-  cli();
+  // cli();
   disable_irq(8);
   send_eoi(8); // Send EOI to RTC IRQ
-  OUTB(0x70, 0x0C);	// select register C
-  INB(0x71);		// just throw away contents
+  outb(0x70, 0x0C);	// select register C
+  inb(0x71);		// just throw away contents
   enable_irq(8);
-  sti();
+  // sti();
 }
