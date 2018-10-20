b79e0f174377ea43009101dad79316a82d4d1cce
Krishnan <rahulk3@illinois.edu>
2018-10-20 14:02:38 -0500

    keyboard doesn't exit interrupt

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -2,6 +2,12 @@
 #include "i8259.h"
 #include "lib.h"
 
+uint32_t status;
+uint32_t scanCode;
+uint32_t prevScanCode;
+int shift;
+int caps;
+
 /* https://wiki.osdev.org/PS/2_Keyboard#Scan_Code_Set_1 */
 
                                     // neither caps lock or shift pressed
@@ -28,40 +34,37 @@ static char scanCodeToChar[3][60] = {{'\0', '\0', '1', '2', '3', '4', '5', '6', 
 
 void KEYBOARD_INIT() {
   enable_irq(1);
+  scanCode = 0;
+  prevScanCode = 0;
+  shift = 0;
+  caps = 0;
   clear();
 }
 
 void KEYBOARD_HANDLER() {
-  asm("pusha");
-  uint32_t prevScanCode = 0;
-  int shift = 0;
-  int caps = 0;
-  while(1) {
-    uint32_t scanCode = inb(0x60);
-    if (scanCode!=0 && scanCode<0x80) {
-      if (prevScanCode!=scanCode) {
-        if (shift==1)
-          printf("%c", scanCodeToChar[2][scanCode]);
-        else if (caps==1)
-          printf("%c", scanCodeToChar[1][scanCode]);
-        else
-          printf("%c", scanCodeToChar[0][scanCode]);
+  while (1) {
+    status = inb(0x64);
+    if (status&0x1) {
+      scanCode = inb(0x60);
+      if (scanCode!=0 && scanCode<0x80) {
+        if (prevScanCode!=scanCode) {
+          if (shift==1)
+            printf("%c", scanCodeToChar[2][scanCode]);
+          else if (caps==1)
+            printf("%c", scanCodeToChar[1][scanCode]);
+          else
+            printf("%c", scanCodeToChar[0][scanCode]);
+        }
       }
-    }
-    if (prevScanCode==0x3A && scanCode == 0xBA) {
-      if (caps==0) caps=1;
-      else caps=0;
-    }
+      if (prevScanCode==0x3A && scanCode == 0xBA) {
+        if (caps==0) caps=1;
+        else caps=0;
+      }
 
-    if (scanCode==0x2A || scanCode==0x36) shift = 1;
-    if (scanCode==0xAA || scanCode==0xB6) shift = 0;
-    prevScanCode = scanCode;
-
-    if (scanCodeToChar[0][scanCode] == '`') {
-      clear();
-      break;
+      if (scanCode==0x2A || scanCode==0x36) shift = 1;
+      if (scanCode==0xAA || scanCode==0xB6) shift = 0;
+      prevScanCode = scanCode;
     }
   }
   send_eoi(1);
-  asm("popa");
 }
