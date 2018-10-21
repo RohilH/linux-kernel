45f36e6603d4f227da2d29dd0d451c76d98a2bd4
Jeng <mjeng2@illinois.edu>
2018-10-20 20:02:18 -0500

    rtc comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -6,17 +6,17 @@ void RTC_INIT() {
   // SRC: https://wiki.osdev.org/RTC
 
   // cli();
-  outb(REG_B, IO_PORT1); // Status Register B / Disable NMI
-  char prev = inb(IO_PORT2);	// Register B value
+  outb(REG_B, IO_PORT1); // Idx --> Reg B / NMI_HANDLER
+  char prev = inb(IO_PORT2); // Obtain Reg A val
   outb(REG_B, IO_PORT1); // Reset idx
-  outb(prev | BIT_SIX_MASK, IO_PORT2); // ORed with 0x40: Register B
+  outb(prev | BIT_SIX_MASK, IO_PORT2); // Reg B bit 6 mask
   // sti();
 
-  // uint8_t rate = 0x0F; // rate must be above 2 and not over 15
-  // outb(0x70, 0x8A); // set index to register A, disable NMI
-  // prev = inb(0x71); // get initial value of register A
-  // outb(0x71, (prev & 0xF0) | rate); //write only our rate to A. Note, rate is the bottom 4 bits.
-  // outb(0x70, 0x8A); // reset index to A
+  // uint8_t rate = 0x0F;
+  // outb(0x70, 0x8A);
+  // prev = inb(0x71);
+  // outb(0x71, (prev & 0xF0) | rate);
+  // outb(0x70, 0x8A);
 
   enable_irq(IRQ_LINE_RTC); // Enable RTC IRQ
   // sti(); // Enable interrupts
