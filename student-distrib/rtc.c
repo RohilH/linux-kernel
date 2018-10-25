a2aafcdbb6e1c2ac733ded014e8f4e6227c46da2
Iyengar <ibi2@illinois.edu>
2018-10-25 18:24:46 -0500

    Finished RTC. Need to make a better test case setup.

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -2,6 +2,8 @@
 #include "i8259.h"
 #include "lib.h"
 
+volatile int interruptFlag;
+
 void RTC_INIT() {
   // SRC: https://wiki.osdev.org/RTC
 
@@ -21,14 +23,94 @@ void RTC_INIT() {
 //  sti(); // Enable interrupts
 }
 
+int RTC_OPEN() {
+  int x = RTC_CHANGE_FREQ(2);
+  if ( x != 0 ) {
+    return -1;
+  }
+  return 0;
+}
+
+int RTC_READ() {
+  disable_irq(1);
+  interruptFlag = 0;
+  while(interruptFlag == 0) {
+    // Spin until interrupt is raised
+  }
+  enable_irq(1);
+  return 0;
+}
+
+int RTC_WRITE(int32_t fd, const void* buf, int32_t nbytes) {
+  if(nbytes != 4) {
+    return -1;
+  }
+  int x = RTC_CHANGE_FREQ(*(int32_t*)buf);
+  if ( x != 0 ) {
+    return -1;
+  }
+  return nbytes;
+}
+
+int RTC_CLOSE() {
+  int x = RTC_CHANGE_FREQ(2);
+  if ( x != 0 ) {
+    return -1;
+  }
+  return 0;
+}
+
+int RTC_CHANGE_FREQ(int32_t inputFreq) {
+  char prev;
+  outb(REG_A, IO_PORT1); // Idx --> Reg B / NMI_HANDLER
+  prev = inb(IO_PORT2); // Obtain Reg A val
+
+  uint8_t rate = translateFrequency(inputFreq); // Translate the input frequency to 8 bits
+  if(rate == 0x00) {
+    return -1;
+  }
+
+  outb(REG_A, IO_PORT1);
+  outb((prev & 0xF0) | rate, IO_PORT2);
+  return 0;
+}
+
+uint8_t translateFrequency(int32_t inputFreq) {
+  switch(inputFreq) {
+      case 2  :
+        return 0x0F;
+      case 4  :
+        return 0x0E;
+      case 8  :
+        return 0x0D;
+      case 16  :
+        return 0x0C;
+      case 32  :
+        return 0x0B;
+      case 64  :
+        return 0x0A;
+      case 128  :
+        return 0x09;
+      case 256  :
+        return 0x08;
+      case 512  :
+        return 0x07;
+      case 1024  :
+        return 0x06;
+      default :
+        return 0x00;
+  }
+}
+
+
 void RTC_HANDLER() {
-  //disable_irq(IRQ_LINE_RTC);
   send_eoi(IRQ_LINE_RTC); // Send EOI to RTC IRQ
   cli();
   outb(REG_C, IO_PORT1);
   inb(IO_PORT2);
-  // test_interrupts();
+  RTC_CHANGE_FREQ(32);
+  test_interrupts();
+  //interruptFlag = 1;
   sti();
   return;
-//  enable_irq(IRQ_LINE_RTC); // Enable RTC IRQ
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -1,8 +1,6 @@
 #ifndef _RTC_H
 #define _RTC_H
-
-extern void RTC_INIT();
-extern void RTC_HANDLER();
+#include "types.h"
 
 #define IO_PORT1 0x70 // Index specifier / Disable NMI
 #define IO_PORT2 0x71 // R/W Port from/to CMO
@@ -12,5 +10,17 @@ extern void RTC_HANDLER();
 #define BIT_SIX_MASK 0x40
 
 #define IRQ_LINE_RTC 8
+
+
+extern void RTC_INIT();
+extern void RTC_HANDLER();
+int RTC_OPEN();
+int RTC_READ();
+int RTC_WRITE(int32_t fd, const void* buf, int32_t nbytes);
+int RTC_CLOSE();
+int RTC_CHANGE_FREQ(int32_t inputFreq);
+uint8_t translateFrequency(int32_t inputFreq);
+
+
 
 #endif
