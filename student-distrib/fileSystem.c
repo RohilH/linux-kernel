2dba51793ee7901579608cd868e5b077bd948c44
Krishnan <rahulk3@illinois.edu>
2018-10-26 15:53:25 -0500

    file system additions

--- student-distrib/fileSystem.c
+++ student-distrib/fileSystem.c
@@ -95,3 +95,35 @@ int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t lengt
 
   return bytesRead;
 }
+
+int32_t file_read (int32_t fd, void* buf, int32_t nBytes) {
+
+}
+
+int32_t file_write (int32_t fd, const void* buf, int32_t nBytes) {
+
+}
+
+int32_t file_open (const uint8_t* fileName) {
+
+}
+
+int32_t file_close (int32_t fd) {
+
+}
+
+int32_t dir_read (int32_t fd, void* buf, int32_t nBytes) {
+
+}
+
+int32_t dir_write (int32_t fd, const void* buf, int32_t nBytes) {
+
+}
+
+int32_t dir_open (const uint8_t* fileName) {
+
+}
+
+int32_t dir_close (int32_t fd) {
+
+}
--- student-distrib/fileSystem.h
+++ student-distrib/fileSystem.h
@@ -31,5 +31,15 @@ extern void fsInit (uint32_t startAddr);
 int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
 int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
-void copyDentry(dentry_t * dest, dentry_t * src);
+
+int32_t file_read (int32_t fd, void* buf, int32_t nBytes);
+int32_t file_write (int32_t fd, const void* buf, int32_t nBytes);
+int32_t file_open (const uint8_t* fileName);
+int32_t file_close (int32_t fd);
+
+int32_t dir_read (int32_t fd, void* buf, int32_t nBytes);
+int32_t dir_write (int32_t fd, const void* buf, int32_t nBytes);
+int32_t dir_open (const uint8_t* fileName);
+int32_t dir_close (int32_t fd);
+
 #endif
