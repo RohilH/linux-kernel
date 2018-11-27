8ecf6953821d6b1744ff20a82091506c0a973ee9
Iyengar <ibi2@illinois.edu>
2018-11-27 11:40:26 -0600

    Commenting out handlers for up arrow and down arrow

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -83,10 +83,10 @@ void KEYBOARD_HANDLER() {
                 backspace();
             }
             else if (scanCode == UP_ARROW_PRESSED) { // Handle up-arrow for commands
-                upArrow();
+                //upArrow();
             }
             else if (scanCode == DOWN_ARROW_PRESSED) {
-                downArrow();
+                //downArrow();
             }
             else if (caps && shift) { // Handle caps and shift
                 addCharToBuffer(scanCode, 3);
